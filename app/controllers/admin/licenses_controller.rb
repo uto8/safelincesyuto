a6af32e9e7247f72d2665d60daff2bb1766d8c7e
uto8 <rb7e8bfa72xe6ee05se@softbank.ne.jp>
2022-04-16 21:13:16 +0900

    資格作成

---
+++ app/controllers/admin/licenses_controller.rb
@@ -0,0 +1,44 @@
+class Admin::LicensesController < ApplicationController
+  def index
+    @licenses = License.page(params[:page]).per(10)
+  end
+
+  def new
+    @license = License.new
+  end
+
+  def create
+    @license = License.create!(license_params)
+    if @license.save
+      flash[:success] = "資格の作成に成功しました。"
+      redirect_to admin_licenses_path
+    else
+      render "new"
+    end
+  end
+
+  def edit
+    @license = License.find(params[:id])
+  end
+
+  def update
+    @license = License.find(params[:id])
+    if @license.update(license_params)
+      flash[:success] = "資格の編集に成功しました。"
+      redirect_to admin_licenses_path
+    else
+      render "new"
+    end
+  end
+
+  def destroy
+    @license=License.find(params[:id])
+    @license.destroy
+    redirect_to admin_licenses_path
+  end
+
+  private
+  def license_params
+    params.require(:license).permit(:name, :fee)
+  end
+end
