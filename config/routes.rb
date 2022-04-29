550f67a2ab0eba45682316cca632616aa5ebd1c2
uto8 <rb7e8bfa72xe6ee05se@softbank.ne.jp>
2022-04-29 09:47:35 +0900

    refs webrs/create-top

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  namespace :admin do
-  end
+  root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :admin do
     get '/dashboard', to: 'pages#index'
