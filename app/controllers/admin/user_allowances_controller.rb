d5c5f25d2c58321310496f459eee177a646fcba2
uto8 <rb7e8bfa72xe6ee05se@softbank.ne.jp>
2022-06-04 22:59:12 +0900

    手当カレンダー作成

--- app/controllers/admin/user_allowances_controller.rb
+++ app/controllers/admin/user_allowances_controller.rb
@@ -1,19 +1,17 @@
 class Admin::UserAllowancesController < ApplicationController
   def index
     @monthly_allowance = UserAllowance.group("DATE_FORMAT(date, '%Y%m')").select("DATE_FORMAT(date, '%Y%m') as month")
-    @allowances = UserAllowance.all
   end
   def list
     @month = params[:month]
-    # @monthly_allowances = UserAllowance.select("DATE_FORMAT(date, '%Y%m') as month")
     @monthly_allowances = UserAllowance.where("DATE_FORMAT(date,'%Y%m') = #{@month}").joins(:user).group(:user_id).select(:name, :user_id)
-    # @monthly_allowance_users = @monthly_allowances.where(month: @month).group(:user_id)
+    @monthly_allowances_first_date = UserAllowance.find_by("DATE_FORMAT(date,'%Y%m') = #{@month}").date 
   end
   def allowance
     @month = params[:month]
     @user_id = params[:id]
     @user = User.find(params[:id])
-    @user_allowances = UserAllowance.all.joins(:license).select(:date,:title)
+    @user_allowances = UserAllowance.where(user_id: @user).joins(:license).select(:date,:title,:license_id,:price)
     @monthly_allowances = UserAllowance.where("(DATE_FORMAT(date,'%Y%m') = #{@month}) AND (user_id = #{@user_id}) ")
     @monthly_allowances_sum = UserAllowance.where("(DATE_FORMAT(date, '%Y%m') = #{@month}) AND (user_id = #{@user_id})").joins(:license).group(:license_id).select("title, sum(price) as price")
   end
