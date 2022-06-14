6833971c1e07bdb2ec70d6334a62c59b8a95fd1a
uto8 <rb7e8bfa72xe6ee05se@softbank.ne.jp>
2022-06-14 22:18:32 +0900

    Herokuのseed変更

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,48 +13,12 @@ User.create!(
       email: 'yuto@gmail.com',
       password: 'password',
       is_admin: true
-    },
-    {
-      name: 'のむらゆうと',
-      email: 'nomura@gmail.com',
-      password: '12345678',
-      is_admin: false
-    },
-    {
-      name: '部長',
-      email: 'butyou@gmail.com',
-      password: '12345678',
-      is_admin: false
-    },
-    {
-      name: '課長',
-      email: 'katyou@gmail.com',
-      password: '12345678',
-      is_admin: false
-    },
-    {
-      name: '係長',
-      email: 'kakarityou@gmail.com',
-      password: '12345678',
-      is_admin: false
     }
   ]
 )
 
 License.create!(
   [
-    {
-      title: '社長',
-      fee: 1000
-    },
-    {
-      title: '社員',
-      fee: 800
-    },
-    {
-      title: 'バイト',
-      fee: 500
-    },
     {
       title: '運転手当',
       fee: 1500
@@ -70,29 +34,6 @@ License.create!(
     {
       title: '隊長手当',
       fee: 4500
-    }
-  ]
-)
-
-Project.create!(
-  [
-    {
-      name: '伝票１',
-      date: '2022-05-01',
-      start_time: '2000-01-01 09:45:00.000000000 +0000',
-      end_time: '2000-01-01 18:45:00.000000000 +0000',
-      leader_id: 2,
-      address: '場所１',
-      supplement: '備考１'
-    },
-    {
-      name: '伝票２',
-      date: '2022-05-01',
-      start_time: '2000-01-01 09:45:00.000000000 +0000',
-      end_time: '2000-01-01 18:45:00.000000000 +0000',
-      leader_id: 1,
-      address: '場所２',
-      supplement: '備考２'
     }
   ]
 )
